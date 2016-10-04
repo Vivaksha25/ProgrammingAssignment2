8d9fd17d5152244d2e28cc6a55f19238c955d399
Vivaksha25 <vivaksha.solanki@gmail.com>
2016-10-04 09:43:39 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix sets  the value for x (as input matrix) and inv as null
+## inv is assigned null, so that on previous value gets erased from memory
 
-## Write a short comment describing this function
+## after setting the value, get/getMatrix function passed the value to the next function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  set<-function(y){
+    x<<-y
+    inv<-NULL
+  }
+  get<-function() x
+  setMatrix<-function(solve) inv<<-solve
+  getMatrix<-function() inv
+  list(set = set, get = get,
+       setMatrix = setMatrix,
+       getMatrix = getMatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will calculate the inverse of the matrix it retrievs from makeCacheMatrix$getMatrix function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv<-x$getMatrix()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data<-x$get()
+  inv<-solve(data)
+  x$setMatrix(inv)
+  inv
 }
